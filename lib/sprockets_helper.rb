66eafe26196fabe147859d71818d62f984cceef6
Amiel Martin <amiel.martin@gmail.com>
2009-10-16 10:47:21 -0700

    make sprockets_include_tag act more like javascript_include_tag, ie accept multiple arguments

--- lib/sprockets_helper.rb
+++ lib/sprockets_helper.rb
@@ -1,9 +1,9 @@
 module SprocketsHelper
-	def sprockets_include_tag(name = nil)
-		if name.nil?
+	def sprockets_include_tag(*names)
+		if names.empty?
 			javascript_include_tag("/sprockets.js")
 		else
-			javascript_include_tag("/sprockets/#{name}")
+			javascript_include_tag(*names.collect{|n| "/sprockets/#{n}"})
 		end
 	end
 end
